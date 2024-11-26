1fd0584fb413ae3ddaefdbb5b6d947e3dd295b13
ngcthao <lyng@oregonstate.edu>
2024-11-25 23:27:40 -0500

    Basic UI Outline

---
+++ news-app/app/page.js
@@ -0,0 +1,23 @@
+import Table from './ui/table.js'
+import OutlinedCard from './ui/settings.js'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid2';
+
+export const metadata = {
+  title: 'NewsApp',
+}
+
+export default function App() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid size={9}>
+        <Table/>
+        </Grid>
+        <Grid size={3}>
+        <OutlinedCard/>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
---
+++ news-app/app/ui/table.js
@@ -0,0 +1,66 @@
+'use client'
+ 
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const doop = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: 'rgb(101, 115, 195)',
+      paper: 'rgb(44, 56, 126)',
+    },
+    text: {
+      primary: '#fff',
+      disabled: '#fff',
+    },
+  }
+});
+
+
+
+export default function NewsTable() {
+  return (
+    <section>
+    <ThemeProvider theme={doop}>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table" className="NewsTable">
+
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell align="right">Title</TableCell>
+            <TableCell align="right">Domain</TableCell>
+            <TableCell align="right">Tags</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+            <TableRow>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+        </TableBody>
+
+      </Table>
+    </TableContainer>
+    </ThemeProvider>
+    </section>
+  );
+}
