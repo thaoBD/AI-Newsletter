7acfdd3f947a2418338f890fc4ac294e40d0d517
ngcthao <lyng@oregonstate.edu>
2024-12-02 19:54:02 -0500

    Implemented react hooks to the form

--- news-app/app/ui/card.js
+++ news-app/app/ui/card.js
@@ -1,3 +1,5 @@
+'use client'
+import {useState} from 'react';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -10,65 +12,176 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import Stack from '@mui/material/Stack';
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
-
-const card = (
-  <React.Fragment>
-
-    <CardContent>
-    <Typography gutterBottom variant="h6" component="div">FILTERS</Typography>
-
-    <Box>
-      <Typography gutterBottom variant="h6" component="div">Tags</Typography>
-      <Box sx={{ width: 500, maxWidth: '100%' }}>
-        <TextField fullWidth label="fullWidth" id="fullWidth" />
-      </Box>
-      <Stack direction="row" spacing={2}>
-      <Button variant="contained">Contained</Button>
-      <Button variant="contained">Contained</Button>
-      </Stack>
-    </Box>
-
-    <Box>
-      <Typography gutterBottom variant="h6" component="div">Websites</Typography>
-      <Box sx={{ width: 500, maxWidth: '100%' }}>
-        <TextField fullWidth label="fullWidth" id="fullWidth" />
-      </Box>
-      <Stack direction="row" spacing={2}>
-      <Button variant="contained">Contained</Button>
-      <Button variant="contained">Contained</Button>
-      </Stack>
-    </Box>
-
-    <Box>
-      <Typography gutterBottom variant="h6" component="div">Subscription</Typography>
-      <FormGroup>
-        <FormControlLabel control={<Switch/>} label="Email" />
-        <FormControlLabel control={<Switch/>} label="Text" />
-      </FormGroup>
-    </Box>
-    </CardContent>
-
-    <CardActions>
-      <Button size="small">Apply Filter</Button>
-      <Button size="small" disabled>Save</Button>
-    </CardActions>
-
-  </React.Fragment>
-);
+// use api routes
 
 export default function OutlinedCard() {
+  const [keyInput, setKeyInput] = useState('');
+  const [domainInput, setDomainInput] = useState('');
+  const [keywords, setKeywords] = useState([]);
+  const [domains, setDomains] = useState([]);
+  const [emailToggle, setEmailToggle] = useState(false);
+  const [textToggle, setTextToggle] = useState(false);
+  const [results, setResults] = useState([]);
+
+  const handleEmailToggleChange = () => {
+    setEmailToggle(prevState => !prevState);
+  };
+
+  const handleTextToggleChange = () => {
+    setTextToggle(prevState => !prevState);
+  };
+
+  // Handle Keywords
+  const handleKeyInputChange = (e) => {
+    setKeyInput(e.target.value);
+  };
+
+  const handleAddKeyword = (e) => {
+    var input = keyInput.trim()
+    if (e.key === 'Enter' && input.trim() !== '') {
+      setKeywords((prevKeywords) => prevKeywords.includes(input) ? prevKeywords : [...prevKeywords, input]);
+      setKeyInput('');
+    }
+  };
+
+  const handleRemoveKeyword = (keyword) => {
+    setKeywords((prevKeywords) =>
+      prevKeywords.filter((item) => item !== keyword)
+    );
+  };
+
+  // Handle Domains
+  const handleDomainInputChange = (e) => {
+    setDomainInput(e.target.value);
+  };
+
+  const handleAddDomain = (e) => {
+    var input = domainInput.trim()
+    if (e.key === 'Enter' && input !== '') {
+      setDomains((prevDomains) => prevDomains.includes(input) ? prevDomains : [...prevDomains, input]);
+      setDomainInput('');
+    }
+  };
+
+  const handleRemoveDomain = (domain) => {
+    setDomains((prevDomains) =>
+      prevDomains.filter((item) => item !== domain)
+    );
+  };
+
+  // Handle Form Submission
+  const handleSubmit = (event) => {
+    // Prevent default form submission
+    event.preventDefault();
+    console.log('Tags:', keywords);
+    console.log('Domains:', domains);
+    console.log('Email:', emailToggle);
+    console.log('text:', textToggle);
+  };
+
+  // const handleSearch = async () => {
+  //   try {
+  //     const response = await fetch('https://your-api-endpoint.com/search', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({ keywords }),
+  //     });
+  //     const data = await response.json();
+  //     console.log('API response:', data); // Handle the API response as needed
+  //   } catch (error) {
+  //     console.error('Error sending data to API:', error);
+  //   }
+  // };
+
+
   return (
     <Box sx={{ minWidth: 275, backgroundColor: '#6573C3'}}>
-      <Card variant="outlined">{card}</Card>
+      <Card variant="outlined"><React.Fragment><FormGroup>
+      
+      <CardContent>
+
+      <Typography gutterBottom variant="h6" component="div">FILTERS</Typography>
+
+      {/* Tags Section */}
+      <Box>
+        <Typography gutterBottom variant="h6" component="div">Tags</Typography>
+
+        <Box sx={{ width: 500, maxWidth: '100%' }}>
+          <TextField 
+          label="search tags"
+          type="text"
+          value={keyInput}
+          onChange={handleKeyInputChange}
+          onKeyDown={handleAddKeyword}
+          placeholder="Enter Keywords Here"/>
+        </Box>
+
+        <Stack direction="row" spacing={2}>
+          {keywords.map((keyword, index) => (
+            <Button 
+            variant="contained"
+            key={index}
+            onClick={() => handleRemoveKeyword(keyword)}
+            >{keyword}
+            </Button>
+          ))}
+        </Stack>
+
+      </Box>
+      
+      {/* Domains Section */}
+      <Box>
+        <Typography gutterBottom variant="h6" component="div">Domains</Typography>
+
+        <Box sx={{ width: 500, maxWidth: '100%' }}>
+          <TextField 
+          label="search domains"
+          type="text"
+          value={domainInput}
+          onChange={handleDomainInputChange}
+          onKeyDown={handleAddDomain}
+          placeholder="Enter Domains Here"/>
+        </Box>
+
+        <Stack direction="row" spacing={2}>
+          {domains.map((domain, index) => (
+            <Button 
+            variant="contained"
+            key={index}
+            onClick={() => handleRemoveDomain(domain)}
+            >{domain}
+            </Button>
+          ))}
+        </Stack>
+
+      </Box>
+      
+      {/* Subscription Section */}
+      <Box>
+        <Typography gutterBottom variant="h6" component="div">Subscription</Typography>
+          <FormControlLabel
+          control={<Switch/>}
+          label="Email"
+          onChange={handleEmailToggleChange}
+          />
+          <FormControlLabel
+          control={<Switch/>}
+          label="Text"
+          onChange={handleTextToggleChange}
+          />
+      </Box>
+      
+      </CardContent>
+      
+      {/* Submission Section */}
+      <CardActions>
+        <Button size="small" onClick={handleSubmit} >Apply Filter</Button>
+        <Button size="small" disabled>Save</Button>
+      </CardActions>
+      
+      </FormGroup></React.Fragment></Card>
     </Box>
   );
 }
