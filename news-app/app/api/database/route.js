29d41b45dc8b4472a38e1d20f294d00dbe02be25
thaoBD <thao@bubble-date.com>
2025-02-27 17:12:03 -0500

    Allow users to save email and phone number

--- news-app/app/api/database/route.js
+++ news-app/app/api/database/route.js
@@ -36,7 +36,7 @@ export async function GET(req) {
   }
 }
 
-// Edit User Preference
+// Edit User Profile
 export async function PUT(req) {
   const params = await req.json()
   const userParams = {
@@ -55,66 +55,12 @@ export async function PUT(req) {
   try {
     // Edit user profile or adds a new user if they do not already exist.
     const userRes = await docClient.send(new UpdateCommand(userParams));
-  } catch (error) {
-    console.error('Error querying DynamoDB:', error);
-    return new Response(JSON.stringify({message: 'Error querying DynamoDB User PUT'}), { status: 500 });
-  }
-
-  const filterParams1 = {
-    TableName: "UserPreferences",
-    Key: {
-      UserId: params.id,
-    },
-    UpdateExpression: "SET #d = :domains, #c = :categories, #k = if_not_exists(#k, :emptyMap), #n = if_not_exists(#n, :emptyMap)",
-    ExpressionAttributeNames: {
-        "#d": "Domains",
-        "#c": "Categories",
-        "#k": "Keys",
-        "#n": "Notifications",
-    },
-    ExpressionAttributeValues: {
-        ":domains": params.domains,
-        ":categories": params.categories,
-        ":emptyMap": {}
-    },
-    ReturnValues: "ALL_NEW",
-  };
-
-  const filterParams2 = {
-    TableName: "UserPreferences",
-    Key: {
-      UserId: params.id,
-    },
-    UpdateExpression: "SET #k.#and = :and, #k.#or = :or, #k.#not = :not, #n.#text = :text, #n.#email = :email",
-    ExpressionAttributeNames: {
-        "#k": "Keys",
-        '#and': "And",
-        '#or': "Or",
-        '#not': "Not",
-        "#n": "Notifications",
-        '#text': "Text",
-        '#email': "Email",
-    },
-    ExpressionAttributeValues: {
-        ":and": params.and,
-        ":or": params.or,
-        ":not": params.not,
-        ":text": params.notifText,
-        ":email": params.notifEmail,
-    },
-    ReturnValues: "ALL_NEW",
-  };
-
-  try {
-    // Edit user's preference, or creates new preference if one does not already exist.
-    const filterRes1 = await docClient.send(new UpdateCommand(filterParams1));
-    const filterRes2 = await docClient.send(new UpdateCommand(filterParams2));
-    return new Response(JSON.stringify(filterRes2), { 
+    return new Response(JSON.stringify(userRes), { 
       status: 200, 
       headers: { 'Content-Type': 'application/json' }
     });
   } catch (error) {
     console.error('Error querying DynamoDB:', error);
-    return new Response(JSON.stringify({message: 'Error querying DynamoDB User Preferences PUT'}), { status: 500 });
+    return new Response(JSON.stringify({message: 'Error querying DynamoDB User PUT'}), { status: 500 });
   }
 }
--- news-app/app/home/page.js
+++ news-app/app/home/page.js
@@ -4,12 +4,83 @@ import OutlinedCard from '../ui/card.js'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
 import {useState} from 'react';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
 import { useSession, signIn, signOut } from "next-auth/react"
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  p: 4,
+};
+
+async function databaseGET(req) {
+  try {
+    const queryParams = new URLSearchParams(req).toString();
+    const url = `/api/database?${queryParams}`;
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+    })
+    return await response.json()
+  } catch (error) {
+    console.log("Error in DynamoDB GET")
+  }
+}
+
+async function databasePUT(req) {
+  try {const response = await fetch('/api/database?', {
+    method: "PUT",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  })
+  return await response.json()
+  } catch (error) {console.error('Error with DynamoDB PUT:', error);}
+}
 
 export default function Home() {
-  const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
   const [data, setData] = useState([]);
+  const [email, setEmail] = useState(1);
+  const [phone, setPhone] = useState(1);
+  const [emailInput, setEmailInput] = useState('');
+  const [phoneInput, setPhoneInput] = useState('');
+  const [open, setOpen] = useState(false);
+  
+  const handleEmailInputChange = (e) => {
+    setEmailInput(e.target.value);
+  };
+
+  const handlePhoneInputChange = (e) => {
+    setPhoneInput(e.target.value);
+  };
+
+  const handleOpen = async () => {
+    if (email == 1 && phone == 1) {
+      const query = {
+        table: "Users",
+        id: session.user.id
+      }
+      const res = await databaseGET(query)
+      setEmail(res.Email)
+      setEmailInput(res.Email)
+      setPhone(res.PhoneNumber)
+      setPhoneInput(res.PhoneNumber)
+    }
+    setEmailInput(email)
+    setPhoneInput(phone)
+    setOpen(true);
+  };
+
+  const handleClose = async () => setOpen(false);
 
   if (status === "loading") {
     return <div>Loading...</div>;
@@ -22,6 +93,23 @@ export default function Home() {
       <button onClick={() => signIn()}>Sign in</button>
       </>
     )
+  }
+
+  const handleSave = async () => {
+    
+    // validate the submission
+
+    if ((email != emailInput) || (phone != phoneInput)) {
+      const query = {
+        id: session.user.id,
+        email: emailInput ? emailInput : null,
+        phoneNumber: phoneInput ? phoneInput : null,
+      }
+  
+      const res = databasePUT(query)
+      setEmail(emailInput ? emailInput : null)
+      setPhone(phoneInput ? phoneInput : null)
+    }
   }
 
   const updateData = (newData) => {
@@ -50,6 +138,27 @@ export default function Home() {
         </Grid>
       </Grid>
       <button onClick={() => signOut()}>Sign Out</button>
+      <button onClick={() => handleOpen()}>Account Settings</button>
+      
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Box sx={style}>
+          <TextField
+            label="Email"
+            onChange={handleEmailInputChange}
+            defaultValue={emailInput}
+          />
+          <TextField
+            label="Phone Number - Only #s"
+            onChange={handlePhoneInputChange}
+            defaultValue={phoneInput}
+          />
+          <button onClick={() => handleSave()}>Save</button>
+        </Box>
+      </Modal>
+
     </Box>
   );
 }
